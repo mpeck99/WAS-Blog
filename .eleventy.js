6986fcc56eccfd6d3bcf77037ddf4230ddd8365d
mlpeck <mlpeck@arkbluecross.com>
2022-02-09 17:27:53 -0600

    creating the base eleventy config

---
+++ .eleventy.js
@@ -0,0 +1,19 @@
+
+const { DateTime } = require("luxon");
+
+module.exports = function (eleventyConfig) {
+  eleventyConfig.addFilter("postDate", (dateObj) => {
+    return DateTime.fromJSDate(dateObj).toLocaleString(DateTime.DATETIME_MED);
+  });
+  eleventyConfig.addWatchTarget('./src/sass/');
+  eleventyConfig.addPassthroughCopy("./src/css");
+    return {
+      dir: {
+        input: "src",
+        output: "public",
+        templateFormats: ['md', '11ty.js']
+      },
+     
+    };
+};
+  
