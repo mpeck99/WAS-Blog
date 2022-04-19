0a9df3073cd1f0c103ff01690522301e3cd2695e
mlpeck <mlpeck@arkbluecross.com>
2022-04-18 21:39:55 -0500

    adding a JS file

--- .eleventy.js
+++ .eleventy.js
@@ -5,9 +5,10 @@ module.exports = function (eleventyConfig) {
   eleventyConfig.addFilter("postDate", (dateObj) => {
     return DateTime.fromJSDate(dateObj).toLocaleString(DateTime.DATE_FULL);
   });
-  eleventyConfig.addWatchTarget('./src/assets/sass/');
+  eleventyConfig.addWatchTarget('./src/assets/sass/**');
   eleventyConfig.addPassthroughCopy("./src/assets/css");
   eleventyConfig.addPassthroughCopy("./src/assets/images");
+  eleventyConfig.addPassthroughCopy('./src/assets/js/');
     return {
       dir: {
         input: "src",
